1dadd351cc895a03050175b83a25c0956552d2f1
jvfe <jvfecav@gmail.com>
2021-02-17 18:41:48 -0300

    feat: Add logic for winning the game or tying

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const gameBoard = (() => {
-  let boardArray = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
+  let boardArray = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
 
   const boardDisplay = document.querySelector(".board-display");
 
@@ -7,10 +7,14 @@ const gameBoard = (() => {
     boardArray[position] = newValue;
   };
 
+  const getBoard = () => {
+    return boardArray;
+  };
+
   const renderDisplay = () => {
-    boardDisplay.textContent = "";
+    boardDisplay.textContent = " ";
     boardArray.map((element, index) => {
-      let cell = document.createElement("div");
+      let cell = document.createElement("button");
       cell.classList.add("cell");
       cell.textContent = element;
       cell.dataset.index = index;
@@ -18,7 +22,7 @@ const gameBoard = (() => {
     });
   };
 
-  return { setCell, renderDisplay };
+  return { getBoard, setCell, renderDisplay };
 })();
 
 const Player = (name, symbol) => {
@@ -42,34 +46,71 @@ const gameController = (() => {
   };
 
   const { Player1, Player2 } = setPlayers("player1", "player2");
-  let currentPlayer = Player2;
+  let currentPlayer = Player1;
+  let gameOver = false;
 
   const playerDiv = document.querySelector(".current-player");
 
-  //
   const setCurrentPlayer = () => {
     currentPlayer = currentPlayer == Player1 ? Player2 : Player1;
     playerDiv.textContent = currentPlayer.name;
   };
 
   const handleTurn = () => {
-    gameBoard.renderDisplay();
-    setCurrentPlayer();
+    if (checkWin()) {
+      gameBoard.renderDisplay();
+      gameOver = true;
+      console.log(`its a win for ${currentPlayer.name}!`);
+    } else if (checkTie()) {
+      gameBoard.renderDisplay();
+      gameOver = true;
+      console.log("its a tie!");
+    } else {
+      setCurrentPlayer();
+      gameBoard.renderDisplay();
+    }
+  };
+
+  const checkWin = () => {
+    const winBoards = [
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    const currentBoard = gameBoard.getBoard();
+
+    const boardOnWin = winBoards.map((i) => i.map((j) => currentBoard[j]));
+
+    return boardOnWin.some((row) =>
+      row.every((cell) => cell === row[0] && cell !== " ")
+    );
+  };
+
+  const checkTie = () => {
+    return gameBoard.getBoard().every((cell) => cell !== " ");
   };
 
   const startGame = () => {
     handleTurn();
-    const cells = document.querySelectorAll(".cell");
+    if (gameOver === false) {
+      const cells = document.querySelectorAll(".cell");
 
-    cells.forEach((cell) => {
-      cell.addEventListener("click", () => {
-        if (cell.textContent === "-") {
-          const pos = parseInt(cell.dataset.index);
-          currentPlayer.play(pos);
-          startGame();
-        }
+      cells.forEach((cell) => {
+        cell.addEventListener("click", () => {
+          if (cell.textContent === " ") {
+            const pos = parseInt(cell.dataset.index);
+            currentPlayer.play(pos);
+            startGame();
+          }
+        });
       });
-    });
+    }
   };
 
   return { startGame };
